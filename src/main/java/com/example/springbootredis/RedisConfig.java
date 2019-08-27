410ae6d6aff8940a63fd3d009a9891d530b67315
admin <421585364@qq.com>
2019-08-27 15:42:36 +0800

    aa

--- src/main/java/com/example/springbootredis/RedisConfig.java
+++ src/main/java/com/example/springbootredis/RedisConfig.java
@@ -10,7 +10,9 @@ import redis.clients.jedis.JedisPoolConfig;
 @Configuration
 public class RedisConfig {
 
-    /*** 1.创建 JedisPoolConfig 对象。在该对象中完成一些链接池配置 **/
+    /***
+     * 1.创建 JedisPoolConfig 对象。在该对象中完成一些链接池配置
+     */
     @Bean
     public JedisPoolConfig jedisPoolConfig(){
         JedisPoolConfig config = new JedisPoolConfig(); //最大空闲数
@@ -20,29 +22,29 @@ public class RedisConfig {
         return config;
     }
 
-        /***
-         * 2.创建 JedisConnectionFactory：配置 redis 链接信息
-         */
-        @Bean
-        public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig config){
-            JedisConnectionFactory factory = new JedisConnectionFactory(); //关联链接池的配置对象
-            factory.setPoolConfig(config); //配置链接 Redis 的信息 //主机地址
-            factory.setHostName("mini3"); //端口
-            factory.setPort(6379);
-            return factory;
-        }
+    /***
+     * 2.创建 JedisConnectionFactory：配置 redis 链接信息
+     */
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(JedisPoolConfig config){
+        JedisConnectionFactory factory = new JedisConnectionFactory(); //关联链接池的配置对象
+        factory.setPoolConfig(config); //配置链接 Redis 的信息 //主机地址
+        factory.setHostName("mini3"); //端口
+        factory.setPort(6379);
+        return factory;
+    }
 
-        /***
-         * 3.创建 RedisTemplate:用于执行 Redis 操作的方法
-         */
-        @Bean
-        public RedisTemplate<String,Object> redisTemplate(JedisConnectionFactory factory){
-            RedisTemplate<String, Object> template = new RedisTemplate<>(); //关联
-            template.setConnectionFactory(factory); //为 key 设置序列化器
-            template.setKeySerializer(new StringRedisSerializer()); //为 value 设置序列化器
-            template.setValueSerializer(new StringRedisSerializer());
-            return template;
-        }
+    /***
+     * 3.创建 RedisTemplate:用于执行 Redis 操作的方法
+     */
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(JedisConnectionFactory factory){
+        RedisTemplate<String, Object> template = new RedisTemplate<>(); //关联
+        template.setConnectionFactory(factory); //为 key 设置序列化器
+        template.setKeySerializer(new StringRedisSerializer()); //为 value 设置序列化器
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
 
 
 }
